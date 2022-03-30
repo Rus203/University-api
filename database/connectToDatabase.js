3e53f6c7722bfecb5c1448352b7b16653aed8b58
Ruslan <c9man24@gmail.com>
2022-03-31 01:26:24 +0300

    feature/error-handler

--- database/connectToDatabase.js
+++ database/connectToDatabase.js
@@ -1,10 +1,11 @@
 const { initRoles } = require('../services/roleService')
 const { sequelize } = require('../models/index')
+const colors = require('colors')
 
-module.exports = async () => {
+module.exports = () => {
   sequelize.sync({ alter: true })
     .then(data => {
       console.log('All models were synchronized successfully.')
       initRoles()
-    })
+    }, error => console.log(colors.red(error)))
 }
---
+++ middleware/errorHandlerMiddleware.js
@@ -0,0 +1,17 @@
+const { StatusCodes, ReasonPhrases } = require('http-status-codes')
+
+module.exports = function (error, request, response, next) {
+  if (error.isOperational) {
+    response.status(error.statusCode).json({
+      status: error.status,
+      message: error.message,
+      statusCode: error.statusCode
+    })
+  } else {
+    response.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      status: 'error',
+      message: ReasonPhrases.INTERNAL_SERVER_ERROR,
+      error: StatusCodes.INTERNAL_SERVER_ERROR
+    })
+  }
+}
